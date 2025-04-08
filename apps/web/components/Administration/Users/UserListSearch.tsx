68649b58bc6c230ffc169ee7527389f69e01137c
Ivo <ivoprovensi1@gmail.com>
2025-04-08 18:10:32 -0400

    feat(users): implement user search functionality with styled components

--- apps/web/components/Administration/Users/UserListSearch.tsx
+++ apps/web/components/Administration/Users/UserListSearch.tsx
@@ -1,18 +1,33 @@
 "use client";
-
-import { TextInput } from "@mantine/core";
+import { FormEvent, useState } from "react";
+import { Box, Button, TextInput } from "@mantine/core";
+import { IconSearch } from "@tabler/icons-react";
 
 import { usePagination } from "@/providers/Pagination";
 
+import classes from "./UserListSearch.module.css";
+
 export function AdministrationUserListSearch() {
-  const { queryText, setQueryText } = usePagination();
+  const [tempQuery, setTempQuery] = useState("");
+  const { setQueryText } = usePagination();
+
+  const doSearch = (e?: FormEvent<HTMLFormElement>) => {
+    e?.preventDefault();
+    setQueryText(tempQuery);
+  };
 
   return (
-    <TextInput
-      label="Search"
-      placeholder="Search by username, email, name"
-      value={queryText}
-      onChange={(e) => setQueryText(e.target.value)}
-    />
+    <Box component="form" onSubmit={doSearch} className={classes.container}>
+      <TextInput
+        className={classes.input}
+        label="Search"
+        placeholder="Search by username, email, name"
+        value={tempQuery}
+        onChange={(e) => setTempQuery(e.target.value)}
+      />
+      <Button className={classes.button} leftSection={<IconSearch />}>
+        Search
+      </Button>
+    </Box>
   );
 }
