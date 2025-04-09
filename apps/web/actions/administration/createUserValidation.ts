2ecc6dd22729084313f6751d91e7340d75886ef0
Ivo <ivoprovensi1@gmail.com>
2025-04-09 18:13:57 -0400

    fix: createUserValidation flaw, again

--- apps/web/actions/administration/createUserValidation.ts
+++ apps/web/actions/administration/createUserValidation.ts
@@ -1,15 +1,7 @@
 "use server";
 import { prisma } from "database";
 
-import { validateAuth } from "../auth/validateAuth";
-
 export async function createUserValidation(userId: string) {
-  const { user } = await validateAuth();
-
-  if (!user) {
-    return "unauthenticated";
-  }
-
   try {
     await prisma.userEmailValidation.delete({
       where: {
