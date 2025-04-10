d010a63f392d9efa7e3cb5fac40282bdcd5fb6bc
Ivo <ivoprovensi1@gmail.com>
2025-04-10 17:22:58 -0400

    fix(pagination): add queryStatus and setQueryStatus to context

--- apps/web/providers/Pagination.tsx
+++ apps/web/providers/Pagination.tsx
@@ -9,10 +9,15 @@ import {
   useMemo,
   useState,
 } from "react";
+import { SIMULATION_STATUS, USER_STATUS } from "database";
 
 interface Props {
   queryText: string;
   setQueryText: Dispatch<SetStateAction<string>>;
+  queryStatus: SIMULATION_STATUS | USER_STATUS | undefined;
+  setQueryStatus: Dispatch<
+    SetStateAction<SIMULATION_STATUS | USER_STATUS | undefined>
+  >;
   take: number;
   setTake: Dispatch<SetStateAction<number>>;
   page: number;
@@ -21,10 +26,12 @@ interface Props {
 
 const Context = createContext<Props>({
   queryText: "",
+  queryStatus: undefined,
   page: 1,
   take: 10,
-  setPage() {},
   setQueryText() {},
+  setQueryStatus() {},
+  setPage() {},
   setTake() {},
 });
 
@@ -41,6 +48,9 @@ export function PaginationProvider({
   defaultTake = 10,
 }: PropsWithChildren<ProviderProps>) {
   const [queryText, setQueryText] = useState("");
+  const [queryStatus, setQueryStatus] = useState<
+    SIMULATION_STATUS | USER_STATUS | undefined
+  >(undefined);
   const [page, setPage] = useState(1);
   const [take, setTake] = useState(defaultTake);
 
@@ -48,12 +58,14 @@ export function PaginationProvider({
     () => ({
       queryText,
       setQueryText,
+      queryStatus,
+      setQueryStatus,
       page,
       setPage,
       take,
       setTake,
     }),
-    [queryText, page, take]
+    [queryText, queryStatus, page, take]
   );
 
   return <Context.Provider value={value}>{children}</Context.Provider>;
